734545c44e5107dde2d200385ef3b1ba3f0e9d1e
aaaaagold <10041465+aaaaagold@users.noreply.github.com>
2019-01-03 16:34:44 +0800

    v 0.2.9t : TODO:expandInfo(?) ;; add:asol:fail lookup

--- debug/ainput-func/gen.py
+++ debug/ainput-func/gen.py
@@ -1,6 +1,6 @@
 #!/bin/python3
 
 def gen(width,strt):
-	for i in range(1,strt): print("B"+str(i)+" B"+str(i-1)+"\n0 0:-1"+str(i*11)+",1111\n\n")
+	for i in range(strt,0,-1): print("B"+str(i)+" B"+str(i+1)+"\n0 0:110"+str(i*1)+",1111\n\n")
 
 gen(0,9)
