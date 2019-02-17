4fb99e362c2a3155a0cc89a7aa71eca01b938738
aaaaagold <10041465+aaaaagold@users.noreply.github.com>
2019-02-17 23:20:56 +0800

    new branch: ArrangeOutputs

---
+++ debug/a15p_arranged.py
@@ -0,0 +1,56 @@
+#!/bin/python3
+import copy
+import sys
+import time
+import random
+
+from shorthand import *
+from amyhead import *
+#from ah import *
+import ab
+
+class board(ab.board):
+	outputOrder=[ 15,14,11,10,13,9,7,6,5,12,5,4,3,2,1,0 ]
+	'''
+		pieceNum.. -> outputOrder[pieceNum]..
+		00 01 02 03
+		04 05 06 07
+		08 09 10 11
+		12 13 14 15
+		->
+		15 14 13 12
+		11 08 07 06
+		10 05 03 02
+		09 04 01 00
+	'''
+	def __init__(self,wh):
+		super(self.__class__,self).__init__(wh)
+	def _output(self,argv=()):
+		t=()
+		tmp=[ t for _ in range(len(self.__board)) ]
+		for loc in range(len(self.__board)):
+			transedOrd=self.__class__.outputOrder[self.__board[loc]] # change order
+			tmp[transedOrd]=(int(loc//self.__wh[0]),int(loc%self.__wh[0]))
+			#tmp[ self.__board[loc] ] = (loc,)
+			#tmp[ self.__board[loc] ] = (int(loc//self.__wh[0]),int(loc%self.__wh[0]))
+		return ( x for v in tmp for x in v )
+	def copy(self):
+		rtv=self.__class__(self.__wh)
+		rtv.__board=self.copyBoard()
+		rtv.__emptyNum=copy.deepcopy(self.__emptyNum)
+		rtv.__emptyAt=copy.deepcopy(self.__emptyAt)
+		return rtv
+	def output(self,argv=()):
+		rtv=[]
+		rtv+=self._output(argv)
+		return rtv
+	def outputs(self,argv=()):
+		rtv=[]
+		rtv+=self._output(argv)
+		rtv+=self.rawBoard()
+		return rtv
+
+def p9(goal,strt):
+	self=p9
+	ab.p9(goal,strt)
+
