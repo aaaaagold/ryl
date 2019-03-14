933084f4b8f13fcbe8d028fa96628f6f4bfb1a1d
from lab <10041465+aaaaagold@users.noreply.github.com>
2019-03-14 17:06:03 +0800

    ADD: toStr: precs, opts

--- demo/atest-15p-astar.py
+++ demo/atest-15p-astar.py
@@ -13,7 +13,7 @@ xxx=Goaltree()
 xxx.fromTxt("ainput-15p-rev/main.txt")
 bbb=board((4,4))
 
-def test(n,qv,gt,hweight=1,opts={}):
+def test(n,qv,gt,hweights=[],opts={}):
 	steps=opts["steps"] if "steps" in opts else 2323
 	stateLimit=opts["stateLimit"] if "stateLimit" in opts else 4095
 	rtv=0 # meaning: step # sum or max
@@ -21,18 +21,29 @@ def test(n,qv,gt,hweight=1,opts={}):
 	
 	arr=[]
 	for i in range(0,n*n,n+1):
-		arr.append([i])
-		s=set()
-		for x in range(i+1,(i//n+1)*n): s.add(x)
-		for x in range(i+n,n*n,n): s.add(x)
-		if len(s)!=0: arr.append(list(s))
+		if i&1:
+			for x in range((i//n+1)*n-1,i-1,-1):
+				arr.append([x])
+			for x in range(i+n,n*n,n):
+				arr.append([x])
+		else:
+			for x in range(n*n+i%n-n,i+n-1,-n):
+				arr.append([x])
+			for x in range(i,(i//n+1)*n):
+				arr.append([x])
+		#arr.append([i])
+		#s=set()
+		#for x in range(i+1,(i//n+1)*n): s.add(x)
+		#for x in range(i+n,n*n,n): s.add(x)
+		#if len(s)!=0: arr.append(list(s))
 	
-	edge=arr[::-1]
+	edge=[x for x in arr[::-1] if len(x)!=0]
+	print(edge)
 	wtbl={}
 	for i in range(len(edge)):
 		arr=edge[i]
 		for e in arr:
-			wtbl[e]=i*hweight
+			wtbl[e]=i*hweights[i]+1
 	print(wtbl)
 	def h(b):
 		rtv=0
@@ -57,27 +68,35 @@ def test(n,qv,gt,hweight=1,opts={}):
 goldRate=(5**0.5-1)/2
 
 class idv:
-	def __init__(self,w=1):
-		self.w=w
+	def __init__(self,w=0,sz=1):
+		self.w=[random.random()*w+1 for _ in range(sz)]
 	def copy(self):
-		rtv=self.__class__(self.w)
+		rtv=self.__class__(sz=len(self.w))
+		rtv.w=[w for w in self.w]
 		return rtv
-	def mutate(self):
-		r=random.random()+goldRate
-		self.w*=r
-		self.w+=random.random()
-		if self.w<0: w=0
-		return self
-	def cross(self,rhs):
-		self.w=(self.w+rhs.w)/2
-		return self
 	def __lt__(self,rhs):
 		return False
+	def reserve(self,sz,w=1):
+		for _ in range(len(self.w),sz):
+			self.w.append(random.random()*w)
+		return self
+	def mutate(self):
+		r=random.random()+goldRate
+		self.w=[random.random()*w+goldRate for w in self.w]
+		self.w=[w+random.random() for w in self.w]
+		self.w=[0 if w<0 else w for w in self.w]
+		return self
+	def cross(self,rhs):
+		for i in range(len(self.w)):
+			if random.random()<0.5:
+				self.w[i]=rhs.w[i]
+		return self
 
 def test2(gt,psize=11):
 	maxstep=2323
-	pop=[ [None,idv()] ]
+	pop=[ [None,idv(),0] ]
 	for n in range(2,7):
+		for p in pop: p[1].reserve(n*n)
 		gs=gt.sets['Final'][0]
 		gs.clear()
 		g=Goal()
@@ -88,7 +107,7 @@ def test2(gt,psize=11):
 			print("size",n)
 			print("batch",_)
 			# split cases
-			qv=[bbb.random().copy() for _ in range((n+_)*2)]
+			qv=[bbb.random().copy() for _ in range(1+(n+_)*2)]
 			print("qv done")
 			# clear results
 			for p in pop: p[0]=None
@@ -98,20 +117,22 @@ def test2(gt,psize=11):
 				newpop=[]
 				newpop+=pop
 				while len(newpop)<psize:
-					ch=random.choice(pop)[1].copy()
+					p=random.choice(pop)
+					ch=p[1].copy()
 					tmp=[None]
 					if len(pop)>1 and random.random()>0.5:
 						ch2=random.choice(pop)[1]
 						tmp+=[ch.cross(ch2)]
 					else:
 						tmp+=[ch.mutate()]
+					tmp+=[p[2]+1]
 					newpop.append(tmp)
 				print("new pop done")
 				for i in range(len(newpop)):
 					p=newpop[i]
 					if not isNone(p[0]): continue
 					print("P",i)
-					res=test(n,qv,gt,hweight=p[1].w,opts={"steps":maxstep})
+					res=test(n,qv,gt,hweights=p[1].w,opts={"steps":maxstep})
 					p[0]=res
 					print(res)
 				print("score done")
