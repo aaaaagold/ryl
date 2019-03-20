ceaa5693563b73c879fb788cd4d9f6bf52c4f7e2
from lab <10041465+aaaaagold@users.noreply.github.com>
2019-03-20 15:30:20 +0800

    mbot sim coefs found

--- demo/ambot.py
+++ demo/ambot.py
@@ -37,7 +37,7 @@ class mb:
 		# TODO: suppose a timeslot = ?ms
 		# L,R
 		m1,m2=m1m2
-		mscale=math.pi*0.25/800 # [-100,100] dt=0.125 * 7 ~ 90 deg # neg*1.1 # notice inertia
+		mscale=math.pi*0.25/800 # [-100,100] dt=0.125 * 7 ~ 90 deg # neg*1.125 # notice inertia
 		D=1 # distance of 2 wheels
 		# common used
 		pi_180=math.pi/180
