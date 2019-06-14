bbaf182a01dd66e1a74cd0af629492f52162714d
OAO <10041465+aaaaagold@users.noreply.github.com>
2019-06-14 12:26:38 +0800

    mbot-long

---
+++ demo-cleaned/ainput-mbot/long.txt.py
@@ -0,0 +1,4 @@
+def pullUR(ov): return abs(ov[1]-2)+abs(ov[2]-2)
+def pullUL(ov): return abs(ov[1])+abs(ov[2]-2)
+def pullDR(ov): return abs(ov[1]-2)+abs(ov[2])
+def pullDL(ov): return abs(ov[1])+abs(ov[2])
